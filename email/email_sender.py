697ffac322f0899b28ecf44f3ea36064916dfc2a
rhaeyx <francis@rhaey.com>
2019-06-06 18:54:36 +0800

    main script

--- email/email_sender.py
+++ email/email_sender.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+from random import randint
+
+"""
+from email_sender import Email
+x = Email(to="adamgavin.services@gmail.com", bcc="bcc.txt", subject="Auto Body Repair Enquiry", text="content.txt", headless=False)
+x.main()
+
+"""
+
 
 class Email:
     def __init__(self,
-                 email="",
-                 password="",
                  to="asd",
                  bcc="asd",
                  subject="Test",
                  text="This is a content test.",
-                 headless=True):
+                 headless=False):
         """
             params:
             email - string
@@ -21,15 +28,17 @@ class Email:
             text - .txt file        
         """
 
-        if not email or not password or not bcc or not to:
+        if not bcc or not to:
             print("Please pass all paremeters...")
             exit()
 
-        self.email = email
-        self.password = password
         self.to = to 
         with open(bcc, "r") as f:
             self.bcc = f.read().split("\n")
+
+        for line in self.bcc:
+            line = line.strip()
+
         self.subject = subject
         with open(text, "r") as f:
             self.text = f.read()
@@ -49,64 +58,53 @@ class Email:
         except:
             print("Something went wrong with opening gmail...")
 
-    def login(self):
-        print("Logging in as", self.email + "...")
-        email_inp = self.chrome.find_element_by_css_selector("#identifierId")
-        email_inp.send_keys(self.email)
-
-        next_btn = self.chrome.find_element_by_css_selector("#identifierNext > content > span")
-        next_btn.click()
-        
-        sleep(3)
-
-        password_inp = self.chrome.find_element_by_css_selector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")
-        password_inp.send_keys(self.password)
-
-        next_btn = self.chrome.find_element_by_css_selector("#passwordNext > content > span")
-        next_btn.click()
-        print("Logged in...")
-
     def send_email(self, bcc):
-        compose = self.chrome.find_element_by_css_selector(".z0 > div")
-        compose.click()
+        self.chrome.get("https://mail.google.com/mail/u/0/h/1modi9lfr1wvl/?&cs=b&pv=tl&v=b")
 
-        sleep(3)
+        sleep(randint(1, 3))
 
         to_inp = self.chrome.find_element_by_name('to')
         to_inp.send_keys(self.to)
         sleep(1)
         to_inp.send_keys('\n')
 
-
-        sleep(3)
-
-        bcc_btn = self.chrome.find_element_by_css_selector(".aA6 > span > span > span:nth-child(2)")
-        bcc_btn.click()
-
-        sleep(3)
+        sleep(randint(1,3))
 
         bcc_inp = self.chrome.find_element_by_name("bcc")
-        bccs = ' '.join(bcc)
+        bccs = ';'.join(bcc)
         print(bccs)
         bcc_inp.send_keys(bccs)
         
-        subject_inp = self.chrome.find_element_by_name("subjectbox")
+        subject_inp = self.chrome.find_element_by_name("subject")
         subject_inp.send_keys(self.subject)
 
-        sleep(3)
+        sleep(randint(1,3))
 
-        text_inp = self.chrome.find_element_by_css_selector('.Ar.Au > div')
+        text_inp = self.chrome.find_element_by_name('body')
         text_inp.send_keys(self.text)
 
-        sleep(3)
+        sleep(randint(1,3))
 
-        send_btn = self.chrome.find_element_by_css_selector(".dC > div")
+        send_btn = self.chrome.find_element_by_name("nvp_bu_send")
         send_btn.click()
         print("Email sent.")
 
+    def start(self):
+        counter = 0
+        while counter < len(self.bcc):
+            if counter != 0 and counter % 60 == 0:
+                print("Taking break...")
+                for _ in range(randint(250, 300)):
+                    sleep(1)
+            bcc_recipients = self.bcc[counter: counter + 3 if counter + 3 < len(self.bcc) - 1 else None]
+            counter += 3
+            self.send_email(bcc_recipients)
+            for _ in range(randint(3, 5)):
+                sleep(1)
+
+
     def main(self):
         input("Enter to start...")
-        print("Email:", self.email)
         print("BCCs:", self.bcc)
         print("Email Subject:", self.subject)
         print("Email Content:", self.text, end='\n')
@@ -117,19 +115,9 @@ class Email:
 
         sleep(5)
 
-        self.login()
+        input("Log in please. Enter to continue.")
 
         sleep(10)
 
-        counter = 0
-        input('Start now...')
-        while counter < len(self.bcc):
-            if counter != 0 and counter % 60 == 0:
-                sleep(300)
-                print("5 minutes break...")
-            bcc_recipients = self.bcc[counter: counter + 3 if counter + 3 < len(self.bcc) - 1 else None]
-            counter += 3
-            print("BCCs:", bcc_recipients)
-            self.send_email(bcc_recipients)
-            for i in range(5):
-                sleep(1)
+        input('Press Enter to start now...')
+        self.start()
